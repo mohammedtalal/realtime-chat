6882ee44b9ada3e85faf85916df502e9c81186a7
Mohammed Talal <mohammed.talal2016@hotmail.com>
2018-10-23 15:49:05 +0200

    dev dep

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 
-const port = 3000
+const port = process.env.PORT || 3000;
 // Start the server
 http.listen(port, () => {
     console.log('Listening on port ' + port)
